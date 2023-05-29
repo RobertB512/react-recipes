f394315f9fafb1ae9de28821a7ecdd538ba9e77e
RobertB512 <rwbcool@gmail.com>
2023-05-29 10:58:45 -0700

    Add search explanation and fix hover effect to back button

--- src/components/Search.js
+++ src/components/Search.js
@@ -10,10 +10,14 @@ export default function Search({value, handleQuery}) {
 	return (
 		<article className="search-area">
 			<form action="">
-				<input placeholder="Search for a recipe" className="recipe-search" type="search" value={value} onChange={handleSearch} />
-				{/* <button className="search-btn" type="submit">
-					Search
-				</button> */}
+				<div className="search-control">
+          <input placeholder="Search for a recipe" className="recipe-search" type="search" value={value} onChange={handleSearch} />
+          {/* <button className="search-btn" type="submit">
+            Search
+          </button> */}
+        </div>
+        <p className="search-explanation">A maximum of 10 recipes will be shown. If your can't find what you're lookin for, try a more specific search.</p>
+
 			</form>
 		</article>
 	);
